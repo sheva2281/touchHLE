e219048bc52ddef4a637244a277b65ccf01f8474
sheva2281 <147552890+sheva2281@users.noreply.github.com>
2025-10-01 10:47:16 +0300

    Update gles_guest.rs

--- src/frameworks/opengles/gles_guest.rs
+++ src/frameworks/opengles/gles_guest.rs
@@ -139,9 +139,21 @@ fn panic_on_gl_errors(gles: &mut dyn GLES) {
 
 // Generic state manipulation
 fn glGetError(env: &mut Environment) -> GLenum {
+    let ignore_gl_errors = env.options.ignore_gl_errors;
     with_ctx_and_mem(env, |gles, _mem| {
         let err = unsafe { gles.GetError() };
         if err != 0 {
+            if ignore_gl_errors {
+147	+
+                log_once!(
+148	+
+                    "Warning: Guest error reporting is ignored for glGetError(), returning 0."
+149	+
+                );
+150	+
+                return 0;
+151	+
+            }
             log!("Warning: glGetError() returned {:#x}", err);
         }
         err
