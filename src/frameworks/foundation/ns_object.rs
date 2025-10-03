2000a67726c8fbde5a42e0a4ed1092b18893a7e6
sheva2281 <147552890+sheva2281@users.noreply.github.com>
2025-10-03 16:44:06 +0300

    Update ns_object.rs

--- src/frameworks/foundation/ns_object.rs
+++ src/frameworks/foundation/ns_object.rs
@@ -105,6 +105,10 @@ pub const CLASSES: ClassExports = objc_classes! {
     this
 }
 
+- (id)initWithCoder:(id)_coder {
+    msg![env; this init]
+}
+    
 - (NSUInteger)retainCount {
     env.objc.get_refcount(this).into()
 }
